fd8e6f26c4c413a32729c355d819e6a1dc63ade2
NatanMkz <natan.n.martins20@gmail.com>
2023-03-29 00:56:32 -0300

    corrigindo geração de tabela de dados do crc, também foi refeito o código para que seja usado apenas os exemplos solicitados pelo professor.

---
+++ TecnicaCRC/Crc32.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecnicaCRC
+{
+    public class Crc32
+    {
+        private const uint polinomio = 0x04C11DB7;
+
+        private uint[] tabela = new uint[256];
+
+        private uint Compute(byte[] bytes)
+        {
+            uint crc = 0xFFFFFFFF;
+
+            foreach (byte b in bytes)
+            {
+                crc = (crc >> 8) ^ tabela[(crc & 0xFF) ^ b];
+            }
+
+            return crc ^ 0xFFFFFFFF;
+        }
+
+        public uint ComputeChecksum(uint[] data)
+        {
+            byte[] bytes = new byte[data.Length * 4];
+            Buffer.BlockCopy(data, 0, bytes, 0, bytes.Length);
+            return Compute(bytes);
+        }
+
+        public void GenerateTable()
+        {
+            for (uint i = 0; i < tabela.Length; i++)
+            {
+                uint crc = i;
+
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((crc & 1) == 1)
+                    {
+                        crc = (crc >> 1) ^ polinomio;
+                    }
+                    else
+                    {
+                        crc >>= 1;
+                    }
+                }
+
+                tabela[i] = crc;
+            }
+        }
+    }
+}
--- TecnicaCRC/Program.cs
+++ TecnicaCRC/Program.cs
@@ -6,37 +6,11 @@ namespace TecnicaCRC
     {
         static void Main(string[] args)
         {
-            // array de bits para proteger
-            byte[] dados = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10 };
-
-            // Tamanho do polinômio CRC
-            const ushort polinomio = 0x1021;
-
-            // Valor inicial do registro CRC
-            ushort register = 0xFFFF;
-
-            // Cálculo do CRC
-            foreach (byte b in dados)
-            {
-                register ^= (ushort)(b << 8);
-
-                for (int i = 0; i < 8; i++)
-                {
-                    if ((register & 0x8000) != 0)
-                    {
-                        register = (ushort)((register << 1) ^ polinomio);
-                    }
-                    else
-                    {
-                        register <<= 1;
-                    }
-                }
-            }
-
-            // Resultado do CRC
-            ushort crc = (ushort)(register ^ 0xFFFF);
-
-            Console.WriteLine("CRC: 0x{0:X4}", crc);
+            uint[] data = { 0x12345678, 0xAABBCCDD, 0xEEFF0011, 0x22334455 };
+            Crc32 crc = new Crc32();
+            crc.GenerateTable();
+            uint checksum = crc.ComputeChecksum(data);
+            Console.WriteLine("Checksum: 0x{0:X8}", checksum);
         }
     }
 }
